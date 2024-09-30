f4aa4a89f5b747dc87d2d270cbf76675fe87eaa4
Pawan <ramtiwari7081@gmail.com>
2024-09-30 14:48:47 +0530

    react icons

--- Calculator-React/src/App.jsx
+++ Calculator-React/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
+import { IoMdHome } from "react-icons/io";
+import { MdOutlineHome } from "react-icons/md";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -19,7 +21,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          <IoMdHome /> {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
@@ -27,9 +29,10 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+        <MdOutlineHome />
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
